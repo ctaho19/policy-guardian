049fd5ff8b575e72b15eab652ad7c8eaad585c8e
failingarchitect <87951422+ctaho19@users.noreply.github.com>
2025-04-20 14:51:34 -0400

    Update monitoring_thresholds.sql

--- tests/monitoring_thresholds.sql
+++ tests/monitoring_thresholds.sql
@@ -7,7 +7,7 @@ select
     metric_name,
     metric_description,
     warning_threshold,
-    alerting_threshold, -- Renamed from ALERT_THRESHOLD
+    alerting_threshold,
     control_executor,
     metric_threshold_start_date,
     metric_threshold_end_date
@@ -15,7 +15,4 @@ from
     etip_db.phdp_etip_controls_monitoring.etip_controls_monitoring_metrics_details
 where 
     metric_threshold_end_date is Null
-    -- Filter for the specific metrics related to CTRL-1077231
-    and monitoring_metric_id in ('MNTR-1077231-T1', 'MNTR-1077231-T2') 
-    # Alternatively, filter by control_id if that's more standard:
-    # and control_id = 'CTRL-1077231' 
+    and control_id = 'CTRL-1077231'
